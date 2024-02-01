c4a212218ec64e665dc5396ddab3335c8fa5c5fc
Bedizz <turkyilmaz.bediz@gmail.com>
2024-02-01 13:44:27 +0100

    /----------------------Application Middleware----------------

--- middlewares/users.js
+++ middlewares/users.js
@@ -6,7 +6,7 @@ export const secure = (req, res, next) => {
     const textToken = process.env.TOKEN
     const {token} = req.body;
     if (!token || token !== textToken) {
-        return res.status(401).json({message: "Unauthorized"})
+        return res.status(403).json({message: "Unauthorized"})
     } else {
         console.log("hello world")
         next();
